fbf69c0422c2596498c87444242c592c87b4177e
javed.iqbal <javed.iqbal@marksman.com.pk>
2020-01-06 19:46:19 +0500

    added userId

--- src/com/vu/controller/UserViewServlet.java
+++ src/com/vu/controller/UserViewServlet.java
@@ -31,7 +31,6 @@ public class UserViewServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doPost(request, response);
 	}
 
